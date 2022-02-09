31d5aedc6d16212c5def2dd45d59edad9af9a065
francesca <fradeluca85@gmail.com>
2022-02-09 18:50:12 +0100

    add table

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,12 +1,32 @@
 @extends('layouts.base')
 
 @section('pageContent')
-<ul>
+<h1>Fumetti in vendita</h1>
+
+<table class="table">
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">Titolo</th>
+        <th scope="col">Descrizione</th>
+        <th scope="col">Serie</th>
+        <th scope="col">Prezzo</th>
+        <th scope="col">Tipo</th>
+      </tr>
+    </thead>
+    <tbody>
     @foreach ($comicsItems as $comicsItem)
-    <li>{{$comicsItem->title}}</li> 
-    @endforeach
-    
-</ul>
+        <tr>
+            <th>{{$comicsItem->id}}</th>
+            <td>{{$comicsItem->title}}</td>
+            <td>{{$comicsItem->description}}</td>
+            <td>{{$comicsItem->series}}</td>
+            <td>{{$comicsItem->price}}</td>
+            <td>{{$comicsItem->type}}</td>
+        </tr>
+      @endforeach
+    </tbody>
+  </table>
 {{-- @dd($comics) --}}
     
 @endsection
