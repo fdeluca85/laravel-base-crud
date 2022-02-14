e79dba89c698b1280eabeb3650283d373f69b331
francesca <fradeluca85@gmail.com>
2022-02-14 19:41:29 +0100

    validation old

--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -10,7 +10,7 @@
     <div class="form-group">
       <label for="title">Titolo</label>
       <small id="title" class="form-text text-muted">Inserisci il titolo</small>
-      <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title" placeholder="Inserisci il titolo" value="{{$comic->title}}">
+      <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title" placeholder="Inserisci il titolo" value="{{old("title") ? old("title") : $comic->title}}">
 
       @error('title')
             <div class="alert alert-danger mt-2">{{ $message }}</div>
@@ -19,7 +19,7 @@
 
     <div class="form-group">
     <label for="series">Serie</label>
-      <input type="text" class="form-control @error('series') is-invalid @enderror" id="series" name="series" placeholder="Inserisci la serie" value="{{$comic->series}}">
+      <input type="text" class="form-control @error('series') is-invalid @enderror" id="series" name="series" placeholder="Inserisci la serie" value="{{old("series") ? old("series") : $comic->series}}">
       @error('series')
             <div class="alert alert-danger mt-2">{{ $message }}</div>
         @enderror
@@ -27,7 +27,7 @@
 
     <div class="form-group">
         <label for="sale_date">Data di vendita</label>
-        <input input type="datetime" class="form-control @error('sale_date') is-invalid @enderror" id="sale_date" name="sale_date" value="{{$comic->sale_date}}">
+        <input input type="datetime" class="form-control @error('sale_date') is-invalid @enderror" id="sale_date" name="sale_date" value="{{old("sale_date") ? old("sale_date") : $comic->sale_date}}">
         @error('sale_date')
             <div class="alert alert-danger mt-2">{{ $message }}</div>
         @enderror
@@ -37,8 +37,13 @@
         <label for="type">Tipologia</label>
         <select class="form-control @error('type') is-invalid @enderror" id="type" name="type"">
             <option value="" disabled selected>Definisci la tipologia</option>
-            <option value="comic book" {{$comic->type == "comic book" ? "selected" : ""}}>Comic book</option>
-            <option value="graphic novel" {{$comic->type == "graphic novel" ? "selected" : ""}}>Graphic novel</option>
+           @if (old("type"))
+                <option value="comic book" {{old("type") == "comic book" ? "selected" : ""}}>Comic book</option>
+                <option value="graphic novel" {{old("type") == "graphic novel" ? "selected" : ""}}>Graphic novel</option>
+            @else
+                <option value="comic book" {{$comic->type == "comic book" ? "selected" : ""}}>Comic book</option>
+                <option value="graphic novel" {{$comic->type == "graphic novel" ? "selected" : ""}}>Graphic novel</option>
+            @endif
         </select>
         @error('type')
             <div class="alert alert-danger mt-2">{{ $message }}</div>
@@ -47,7 +52,7 @@
 
     <div class="form-group">
         <label for="thumb">Immagine</label>
-        <input type="text" class="form-control @error('thumb') is-invalid @enderror" id="thumb" name="thumb" placeholder="Inserisci url immagine" value="{{$comic->thumb}}">
+        <input type="text" class="form-control @error('thumb') is-invalid @enderror" id="thumb" name="thumb" placeholder="Inserisci url immagine" value="{{old("thumb") ? old("thumb") : $comic->thumb}}">
         @error('thumb')
             <div class="alert alert-danger mt-2">{{ $message }}</div>
         @enderror
@@ -55,7 +60,7 @@
 
     <div class="form-group">
         <label for="price">Prezzo</label>
-        <input type="number" class="form-control @error('price') is-invalid @enderror" id="price" name="price" placeholder="Inserisci il prezzo" value="{{$comic->price}}">
+        <input type="number" class="form-control @error('price') is-invalid @enderror" id="price" name="price" placeholder="Inserisci il prezzo" value="{{old("price") ? old("price") : $comic->price}}">
         @error('price')
             <div class="alert alert-danger mt-2">{{ $message }}</div>
         @enderror
@@ -63,7 +68,7 @@
 
     <div class="form-group">
         <label for="description">Descrizione</label>
-        <textarea class="form-control @error('description') is-invalid @enderror" id="description" name="description" rows="5" placeholder="Inserisci una descrizione"> {{$comic->description}}"</textarea>
+        <textarea class="form-control @error('description') is-invalid @enderror" id="description" name="description" rows="5" placeholder="Inserisci una descrizione"> {{old("description") ? old("description") : $comic->description}}"</textarea>
         @error('description')
             <div class="alert alert-danger mt-2">{{ $message }}</div>
         @enderror
